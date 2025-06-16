418815674c8c44ef04b71cb1920a688950be6e24
avadakedavra-wp <wirachit.p11@gmail.com>
2025-06-17 02:11:10 +0700

    Update Homebrew formula for v0.2.0 release
    - Updated URL to v0.2.0 tag
    - Added correct SHA256 hash for source tarball
    - Ready for production use with Windows support

--- homebrew-fazrepo/Formula/fazrepo.rb
+++ homebrew-fazrepo/Formula/fazrepo.rb
@@ -2,7 +2,7 @@ class Fazrepo < Formula
   desc "A fast CLI tool to check package manager versions"
   homepage "https://github.com/avadakedavra-wp/fazrepo"
   url "https://github.com/avadakedavra-wp/fazrepo/archive/v0.2.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "23c3743003886919f73b92f5ffcab91b24ff8d0ce91e24dfc72b9ae43718a794"
   license "MIT"
 
   depends_on "rust" => :build
